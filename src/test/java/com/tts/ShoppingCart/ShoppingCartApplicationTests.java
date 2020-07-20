f351233549fa486fa65cd08e8cc869341c529964
Paul Holt <32377498+guitarpckr@users.noreply.github.com>
2020-07-20 12:36:05 -0400

    add base files

---
+++ src/test/java/com/tts/ShoppingCart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.ShoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
